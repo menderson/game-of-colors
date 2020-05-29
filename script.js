07e1c53303e3d0c1e84a87d345291914fa85870c
menderson <menderson07@gmail.com>
2020-05-28 21:55:11 -0300

    version 1.0

--- script.js
+++ script.js
@@ -1,84 +1,118 @@
-    var gameOn = true;
-    var score = 0;
-    var time = 12;
-    document.getElementById("rule").innerHTML = "Clique no circulo com a cor diferente antes que o tempo acabe";
-    diferentColorId = getRandom(1, 10);
-    changeColor(diferentColorId);
+var matrixOfCircles = document.querySelector('#app');
+var level = 2;
+var numberOfCircles = 4;
+var idCircleWithDiferentColor;
+var gameOn = true;
+var score = 0;
+var time = 12;
+var red, green, blue;
 
-    function getRandom(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min)) + min;
-    };
+renderCircles();
+changeColor();
 
-    function changeColor(id) {
-      var button1 = document.getElementById('1');
-      var button2 = document.getElementById('2');
-      var button3 = document.getElementById('3');
-      var button4 = document.getElementById('4');
-      var button5 = document.getElementById('5');
-      var button6 = document.getElementById('6');
-      var button7 = document.getElementById('7');
-      var button8 = document.getElementById('8');
-      var button9 = document.getElementById('9');
-      var red = getRandom(0, 255);
-      var green = getRandom(0, 255);
-      var blue = getRandom(0, 255);
-      button1.style.background = "rgb(" + red + "," + green + "," + blue + ")";
-      button2.style.background = "rgb(" + red + "," + green + "," + blue + ")";
-      button3.style.background = "rgb(" + red + "," + green + "," + blue + ")";
-      button4.style.background = "rgb(" + red + "," + green + "," + blue + ")";
-      button5.style.background = "rgb(" + red + "," + green + "," + blue + ")";
-      button6.style.background = "rgb(" + red + "," + green + "," + blue + ")";
-      button7.style.background = "rgb(" + red + "," + green + "," + blue + ")";
-      button8.style.background = "rgb(" + red + "," + green + "," + blue + ")";
-      button9.style.background = "rgb(" + red + "," + green + "," + blue + ")";
-      var diferentButton = document.getElementById(id);
-      red = red + (50 - score);
-      green = green + (50 - score);
-      blue = blue + (50 - score);
-      diferentButton.style.background = "rgb(" + red + "," + green + "," + blue + ")";
-    };
+document.getElementById("rule").innerHTML = "Clique no circulo com a cor diferente";
 
-    function changeId() {
-      diferentColorId = getRandom(1, 10);
-      return diferentColorId;
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+function generateColor() {
+    red = getRandom(0, 255);
+    green = getRandom(0, 255);
+    blue = getRandom(0, 255);
+}
+
+function generateAproximateColor() {
+    red = red + (50 - score);
+    green = green + (50 - score);
+    blue = blue + (50 - score);
+}
+
+function changeColor() {
+    generateColor();
+    for (i = 1; i <= numberOfCircles; i++) {
+        document.getElementById(i).style.background = "rgb(" + red + "," + green + "," + blue + ")";
     }
+    generateAproximateColor();
+    document.getElementById(changeId()).style.background = "rgb(" + red + "," + green + "," + blue + ")";
+};
 
-    function verifyId(id) {
-      if (diferentColorId === id && gameOn==true) {
-        score++;
-        document.getElementById("score").innerHTML = "Pontuação: "+score;
-        if (time < 6) {
-          time += 6;
-          document.getElementById("time").innerHTML = "Tempo: " + time;
-        }
-        else {
-          time = 12;
-          document.getElementById("time").innerHTML = "Tempo: " + time;
-        }
-        id = changeId();
-        changeColor(id);
-      } else {
-        if (time > 5) time -= 5;
-        else {
-            gameOn = false;
-          time = 0;
-          document.getElementById("rule").innerHTML = "Fim de Jogo";
-        }
-        document.getElementById("time").innerHTML = "Tempo: " + time;
-      }
-    };
+function changeId() {
+    idCircleWithDiferentColor = getRandom(1, (numberOfCircles + 1));
+    return idCircleWithDiferentColor;
+}
 
-    setInterval(function () {
-      if (time > 0) {
+function updateScore() {
+    score++;
+    if (score % 10 === 0) {
+        levelUp();
+    }
+    document.getElementById("score").innerHTML = "Pontuação: " + score;
+}
+
+function levelUp() {
+    if (level < 6) {
+        level++;
+        numberOfCircles = level * level;
+        renderCircles();
+    }
+}
+
+function updateTime() {
+    if (time < 6) time += 6;
+    else time = 12;
+    document.getElementById("time").innerHTML = "Tempo: " + time;
+}
+
+function decreaseTime() {
+    if (time > 5) time -= 5;
+    else endGame();
+    document.getElementById("time").innerHTML = "Tempo: " + time;
+}
+
+function endGame() {
+    gameOn = false;
+    time = 0;
+    document.getElementById("rule").innerHTML = "Fim de Jogo";
+}
+
+function verifyId(idSelectedCircle) {
+    if (idCircleWithDiferentColor === idSelectedCircle && gameOn == true) {
+        updateScore();
+        updateTime();
+        changeColor(changeId());
+    } else decreaseTime();
+};
+
+setInterval(function () {
+    if (time > 0) {
         time--;
         document.getElementById("time").innerHTML = "Tempo: " + time;
-      }
-      else {
-          gameOn = false;
+    }
+    else {
+        gameOn = false;
         score = 0;
         document.getElementById("rule").innerHTML = "Fim de Jogo";
-        
-      }
-    }, 1000);
+    }
+}, 1000);
+
+function renderCircles() {
+    matrixOfCircles.innerHTML = '';
+    var circlerPerLine = Math.sqrt(numberOfCircles);
+    var id = 0;
+    for (i = 1; i <= circlerPerLine; i++) {
+        var lineOfCircle = document.createElement('tr')
+        for (j = 1; j <= circlerPerLine; j++) {
+            id++;
+            var circleElement = document.createElement('td');
+            var circleButton = document.createElement('button')
+            circleButton.setAttribute('id', id)
+            circleButton.setAttribute('onclick', 'verifyId(' + id + ')')
+            circleElement.appendChild(circleButton);
+            lineOfCircle.appendChild(circleElement);
+        }
+        matrixOfCircles.appendChild(lineOfCircle);
+    }
+}
